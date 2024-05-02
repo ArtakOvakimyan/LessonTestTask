b301577c27cb88a0df1952d9e9169454cb00c268
Artak Ovakimyan <ovakimyanartak@yandex.ru>
2024-05-02 19:48:23 +0500

    wrong and correct answers test separation

--- src/test/java/bot/BotTest.java
+++ src/test/java/bot/BotTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class BotTest {
     @Test
-    public void testCommandTest() {
+    public void rightTestCommandTest() {
         FakeBot bot = new FakeBot();
         BotLogic botLogic = new BotLogic(bot);
         User user = new User(111L);
@@ -23,6 +23,28 @@ public class BotTest {
         Assert.assertEquals("Тест завершен", bot.getMessages().get(4));
 
     }
+
+    @Test
+    public void wrongTestCommandTest() {
+        FakeBot bot = new FakeBot();
+        BotLogic botLogic = new BotLogic(bot);
+        User user = new User(111L);
+
+        botLogic.processCommand(user, "/test");
+
+        Assert.assertEquals("Вычислите степень: 10^2", bot.getMessages().get(0));
+        botLogic.processCommand(user, "12");
+        Assert.assertEquals("Вы ошиблись, верный ответ: 100", bot.getMessages().get(1));
+        Assert.assertEquals(1, user.getWrongAnswerQuestions().size());
+        Assert.assertEquals("Сколько будет 2 + 2 * 2", bot.getMessages().get(2));
+        botLogic.processCommand(user, "2003");
+        Assert.assertEquals("Вы ошиблись, верный ответ: 6", bot.getMessages().get(3));
+        Assert.assertEquals(2, user.getWrongAnswerQuestions().size());
+
+        Assert.assertEquals("Тест завершен", bot.getMessages().get(4));
+
+    }
+
     @Test
     public void notifyCommandTest() throws InterruptedException {
         FakeBot bot = new FakeBot();
@@ -48,5 +70,6 @@ public class BotTest {
         User user = new User(111L);
 
         botLogic.processCommand(user, "/repeat");
+        //TODO
     }
 }
